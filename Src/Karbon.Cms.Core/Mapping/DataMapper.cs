f3aca14594f775443628af324f01c7b15e7f68dc
Matt <Matt@MBP13-PC.Home>
2013-05-07 19:25:01 +0100

    Added DataMapper class

---
+++ Src/Karbon.Cms.Core/Mapping/DataMapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Karbon.Cms.Core.Models;
+
+namespace Karbon.Cms.Core.Mapping
+{
+    public class DataMapper
+    {
+        /// <summary>
+        /// Maps the specified data an entity.
+        /// </summary>
+        /// <typeparam name="TEntityType">The entity type.</typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        public TEntityType Map<TEntityType>(TEntityType entity, IDictionary<string, string> data)
+            where TEntityType : Entity
+        {
+            var baseType = typeof (Entity);
+            var baseProps = baseType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            
+            var type = typeof (TEntityType);
+            var props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => baseProps.All(y => y.Name != x.Name));
+
+            foreach (var dataKey in data.Keys)
+            {
+                var prop = props.SingleOrDefault(x => x.Name == dataKey && x.CanWrite);
+                if (prop != null)
+                {
+                    // Found property, so attempt to set it
+                    TypeConverter typeConverter = null;
+
+                    var typeConverterAttr = prop.GetCustomAttribute<TypeConverterAttribute>();
+                    if(typeConverterAttr != null)
+                    {
+                        var typeConverterType = Type.GetType(typeConverterAttr.ConverterTypeName);
+                        if (typeConverterType != null)
+                            typeConverter = Activator.CreateInstance(typeConverterType) as TypeConverter;
+                    }
+
+                    if (typeConverter == null)
+                        typeConverter = TypeDescriptor.GetConverter(prop.PropertyType);
+
+                    var propValue = typeConverter.ConvertFromString(data[dataKey]);
+                    prop.SetValue(entity, propValue);
+                }
+                else
+                {
+                    // No property so add to data collection
+                    if (entity.Data.ContainsKey(dataKey))
+                        entity.Data[dataKey] = data[dataKey];
+                    else
+                        entity.Data.Add(dataKey, data[dataKey]);
+                }
+            }
+
+            return entity;
+        }
+    }
+}
---
+++ Src/Karbon.Cms.Tests/Mapping/DataMapperTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Karbon.Cms.Core.Mapping;
+using Karbon.Cms.Core.Models;
+using NUnit.Framework;
+
+namespace Karbon.Cms.Tests.Mapping
+{
+    [TestFixture]
+    public class DataMapperTests
+    {
+        [Test]
+        public void Can_Map_Data()
+        {
+            // Arrange
+            var data = new Dictionary<string, string>
+            {
+                {"Title", "Test Title"},
+                {"Description", "Test Description"},
+                {"Date", "2013-01-01"},
+                {"Int", "21"},
+                {"Bool", "True"},
+                {"IntList", "1,2,3"},
+                {"Misc", "Test Misc"}
+            };
+            var content = new TestContent();
+
+            // Act
+            content = new DataMapper().Map(content, data);
+
+            // Assert
+            Assert.AreEqual("Test Title", content.Title);
+            Assert.AreEqual("Test Description", content.Description);
+            Assert.AreEqual(new DateTime(2013, 1, 1), content.Date);
+            Assert.AreEqual(21, content.Int);
+            Assert.AreEqual(1, content.Data.Count);
+            Assert.IsTrue(content.Bool);
+            Assert.AreEqual(3, content.IntList.Count);
+            Assert.IsTrue(content.Data.ContainsKey("Misc"));
+            Assert.AreEqual("Test Misc", content.Data["Misc"]);
+        }
+    }
+
+    #region Helper Classes
+
+    public class TestContent : Content
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Int { get; set; }
+        public bool Bool { get; set; }
+
+        [TypeConverter(typeof(IntListTypeConverter))]
+        public IList<int> IntList { get; set; }
+    }
+
+    public class IntListTypeConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+                return true;
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, 
+            System.Globalization.CultureInfo culture, object value)
+        {
+            if (value is string)
+                return ((string)value).Split(',').Select(int.Parse).ToList();
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+
+    #endregion
+}
