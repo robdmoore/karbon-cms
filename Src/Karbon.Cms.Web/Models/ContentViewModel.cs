38d37c5a5c4c3975012436d4bb7205116aefb392
Matt <Matt@MBP13-PC.Home>
2013-05-08 21:07:50 +0100

    Added base class for views to give access to Content and Site models

---
+++ Src/Karbon.Cms.Web/Models/ContentViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Karbon.Cms.Core.Models;
+
+namespace Karbon.Cms.Web.Models
+{
+    public class ContentViewModel<TContentType, TSiteType>
+        where TContentType : IContent
+        where TSiteType : ISite
+    {
+        public TContentType Content { get; private set; }
+        public TSiteType Site { get; private set; }
+
+        public ContentViewModel(TContentType content,
+            TSiteType site)
+        {
+            Content = content;
+            Site = site;
+        }
+    }
+
+    public class ContentViewModel<TContentType> : ContentViewModel<TContentType, Site>
+        where TContentType : IContent
+    {
+        public ContentViewModel(TContentType content, Site site) 
+            : base(content, site)
+        { }
+    }
+
+    public class ContentViewModel : ContentViewModel<Content, Site>
+    {
+        public ContentViewModel(Content content, Site site) 
+            : base(content, site)
+        { }
+    }
+}
---
+++ Src/Karbon.Cms.Web/Mvc/ContentView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Karbon.Cms.Core.Models;
+using Karbon.Cms.Web.Models;
+
+namespace Karbon.Cms.Web.Mvc
+{
+    public abstract class ContentView<TContentType, TSiteType> : WebViewPage<ContentViewModel<TContentType, TSiteType>>
+        where TContentType : IContent
+        where TSiteType : ISite
+    {
+        protected override void SetViewData(ViewDataDictionary viewData)
+        {
+            if(viewData.Model is TContentType)
+            {
+                viewData.Model = new ContentViewModel<TContentType, TSiteType>((TContentType) viewData.Model,
+                    default(TSiteType)); // TODO: Populate site model
+            }
+
+            base.SetViewData(viewData);
+        }
+    }
+
+    public abstract class ContentView<TContentType> : ContentView<TContentType, Site>
+        where TContentType : IContent
+    { }
+
+    public abstract class ContentView : ContentView<Content, Site>
+    { }
+}
