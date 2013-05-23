53e36181916e28112a860e60fe9f2fc1619d4f24
Matt <Matt@MBP13-PC.Home>
2013-05-23 16:34:25 +0100

    Moved entity extensions into seperate extensions file

---
+++ Src/Karbon.Cms.Core/Extensions/EntityApiExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Karbon.Cms.Core.Models;
+
+namespace Karbon.Cms.Core
+{
+    public static class EntityApiExtensions
+    {
+        /// <summary>
+        /// Gets the value for the given key.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static string Get(this IEntity content, string key, string defaultValue = "")
+        {
+            string value;
+            return content.TryGet(key, out value) && !string.IsNullOrEmpty(value)
+                ? value
+                : defaultValue;
+        }
+
+        /// <summary>
+        /// Gets the value for the given key.
+        /// </summary>
+        /// <typeparam name="TValueType">The type of the value type.</typeparam>
+        /// <param name="content">The content.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static TValueType Get<TValueType>(this IEntity content, string key, TValueType defaultValue = default(TValueType))
+        {
+            TValueType value;
+            return content.TryGet(key, out value) && !EqualityComparer<TValueType>.Default.Equals(value, default(TValueType))
+                ? value
+                : defaultValue;
+        }
+
+        /// <summary>
+        /// Gets the value for the given key.
+        /// </summary>
+        /// <typeparam name="TValueType">The type of the value type.</typeparam>
+        /// <typeparam name="TConverterType">The type of the converter type.</typeparam>
+        /// <param name="content">The content.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static TValueType Get<TValueType, TConverterType>(this IEntity content, string key, TValueType defaultValue = default(TValueType))
+            where TConverterType : TypeConverter
+        {
+            TValueType value;
+            return content.TryGet<TValueType, TConverterType>(key, out value) && !EqualityComparer<TValueType>.Default.Equals(value, default(TValueType))
+                ? value
+                : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to get the value for the given key.
+        /// </summary>
+        /// <typeparam name="TValueType">The type of the value type.</typeparam>
+        /// <param name="content">The content.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static bool TryGet<TValueType>(this IEntity content, string key, out TValueType value)
+        {
+            value = default(TValueType);
+
+            if (!content.Data.ContainsKey(key))
+                return false;
+
+            try
+            {
+                var tmpValue = content.Data[key];
+                var converter = TypeDescriptor.GetConverter(typeof(TValueType));
+                value = (TValueType)converter.ConvertFromString(tmpValue);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Tries to get the value for the given key.
+        /// </summary>
+        /// <typeparam name="TValueType">The type of the value type.</typeparam>
+        /// <typeparam name="TConverterType">The type of the converter type.</typeparam>
+        /// <param name="content">The content.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static bool TryGet<TValueType, TConverterType>(this IEntity content, string key, out TValueType value)
+            where TConverterType : TypeConverter
+        {
+            value = default(TValueType);
+
+            if (!content.Data.ContainsKey(key))
+                return false;
+
+            try
+            {
+                var tmpValue = content.Data[key];
+                var converter = Activator.CreateInstance(typeof(TConverterType)) as TypeConverter;
+                if (converter == null)
+                    return false;
+
+                value = (TValueType)converter.ConvertFromString(tmpValue);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
